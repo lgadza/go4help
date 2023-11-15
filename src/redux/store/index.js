ef99746b7b5b643a5e00dd29f2f66a7acfde7e2b
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-11-15 13:33:50 +0100

    started

---
+++ src/redux/store/index.js
@@ -0,0 +1,32 @@
+import {
+    configureStore,
+    combineReducers,
+    getDefaultMiddleware,
+  } from "@reduxjs/toolkit";
+  import localStorage from "redux-persist/lib/storage";
+  import { persistReducer, persistStore } from "redux-persist";
+  import { encryptTransform } from "redux-persist-transform-encrypt";
+  const secretKey=process.env.REDUX_SECRET_KEY
+const persistConfig = {
+    key: "root",
+    storage: localStorage,
+    transforms: [
+      encryptTransform({
+        secretKey: "secretKey",
+      }),
+    ],
+  };
+  const bigReducer = combineReducers({
+
+  })
+const persistedReducer = persistReducer(persistConfig, bigReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+export default store;
+export const persistor = persistStore(store);
